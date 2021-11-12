fe54b9e557a55a4eb164a9c9835898bfd7a71d2d
Seline van Dijke <seline.vandijke98@gmail.com>
2021-11-12 10:24:21 +0100

    Opdracht filmzoeker gemaakt

---
+++ script.js
@@ -0,0 +1,84 @@
+const addMoviesToDom = (movies) => {
+
+    const movieList = document.getElementById("movie-list");
+        movieTitles = movies.map(movies => {
+        const movieTitles = movies.Title;
+        const moviePoster = movies.Poster;
+        const movieId = movies.imdbID;
+
+        const addPosterImg = document.createElement("img");
+        const newLi = document.createElement("li");
+        const addA = document.createElement("a");   
+            
+            movieList.appendChild(newLi);
+            newLi.innerHTML = movieTitles;
+            newLi.appendChild(addA);
+            addA.appendChild(addPosterImg);
+            addPosterImg.src = moviePoster;
+            
+            const addLinkToPoster = () => {
+                addA.href = "https://www.imdb.com/title/" + movieId;
+            }
+            addLinkToPoster();
+                
+        });
+    
+}
+
+const getRadioButtons = document.getElementsByName("select-movie");
+getRadioButtons.forEach((radiobtn) => {
+    
+    radiobtn.addEventListener('change', (event) => {
+
+        const handleOnChangeEvent = () => {
+            switch (event.target.getAttribute("id")) {
+                case 'latest-movies':
+                    filterLatestMovies();
+                    break;
+                case 'avenger-movies':
+                    filterMovies("Avengers");
+                    break;
+                case 'xmen-movies': 
+                    filterMovies("X-Men");
+                    break;
+                case 'princess-movies':
+                    filterMovies("Princess");
+                    break;
+                case 'batman-movies':
+                    filterMovies("Batman");
+            }
+
+        }
+        handleOnChangeEvent()
+
+    })
+    
+})
+
+const removeMovies = () => {
+    const parent = document.getElementById("main-list").getElementsByTagName("ul")[0];
+    parent.innerHTML = "";
+}
+
+
+const filterMovies = (wordInMovieTitle) => {
+    const filteredMovies = movies.filter((movie) => {
+        return movie.Title.includes(wordInMovieTitle)
+        
+    })
+    removeMovies();
+    console.log(addMoviesToDom(filteredMovies))
+
+}
+
+const filterLatestMovies = () => {
+    const filteredLatestMovies = movies.filter((movie) => {
+        return movie.Year > 2014
+    })
+    removeMovies();
+    console.log(addMoviesToDom(filteredLatestMovies))
+}
+
+
+
+// https://www.imdb.com/title/tt1877832/
